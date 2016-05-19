da5f84df1bf7ef02dc109f4f6c3470c660d01ef1
kiu <vn.ngocdung@gmail.com>
2016-05-19 08:56:36 +0700

    fix footer link

---
+++ Block/Html/Footer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mageplaza\Blog\Block\Html;
+
+use \Magento\Framework\View\Element\Template\Context;
+use \Mageplaza\Blog\Helper\Data;
+
+class Footer extends \Magento\Framework\View\Element\Html\Link
+{
+	protected $helper;
+
+	public function __construct(
+		Context $context,
+		Data $helper,
+		array $data = []
+	)
+	{
+		$this->helper = $helper;
+		parent::__construct($context, $data);
+	}
+
+	public function getHref()
+	{
+		return $this->helper->getBlogUrl('');
+	}
+}
