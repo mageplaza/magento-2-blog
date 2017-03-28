050bbb4281e45684324fc07d4dc831c7586162e7
Tam <Tam>
2017-03-28 17:01:24 +0700

    Zoom image: fancybox

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    paths: {
+        migrate: 'Mageplaza_Blog/js/jquery-migrate-1.2.1.min',
+        fancybox: 'Mageplaza_Blog/js/jquery.fancybox-1.3.4.pack.min'
+    },
+    shim:{
+        fancybox: ['jquery']
+    }
+};
