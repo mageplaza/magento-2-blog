68b1731cbf13a810747d576a96c3e7726138d7e4
Khánh PK <Khnh PK>
2017-04-24 11:57:18 +0700

    complete comment engine

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,7 +2,7 @@ var config = {
     paths: {
         migrate: 'Mageplaza_Blog/js/lib/jquery-migrate-1.2.1.min',
         fancybox: 'Mageplaza_Blog/js/lib/jquery.fancybox-1.3.4.pack.min',
-        mpblog: 'Mageplaza_Blog/js/mpblog',
+        comment: 'Mageplaza_Blog/js/comment',
         autocomplete: 'Mageplaza_Blog/js/lib/jquery.autocomplete.min'
     },
     shim:{
