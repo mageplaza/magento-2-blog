25ce070988e4d13974d46b7e8bda75f484daf0f6
duc hoang <hoangnd@mageplaza.com>
2017-04-28 23:42:27 +0700

    -fix bugs

---
+++ Block/Adminhtml/Author/Helper/Image.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Blog
+ * @copyright   Copyright (c) 2016 Mageplaza (http://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+namespace Mageplaza\Blog\Block\Adminhtml\Author\Helper;
+
+/**
+ * @method string getValue()
+ */
+class Image extends \Magento\Framework\Data\Form\Element\Image
+{
+    /**
+     * Post image model
+     *
+     * @var \Mageplaza\Blog\Model\Post\Image
+     */
+	public $imageModel;
+
+    /**
+     * constructor
+     *
+     * @param \Mageplaza\Blog\Model\Post\Image $imageModel
+     * @param \Magento\Framework\Data\Form\Element\Factory $factoryElement
+     * @param \Magento\Framework\Data\Form\Element\CollectionFactory $factoryCollection
+     * @param \Magento\Framework\Escaper $escaper
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param array $data
+     */
+    public function __construct(
+        \Mageplaza\Blog\Model\Author\Image $imageModel,
+        \Magento\Framework\Data\Form\Element\Factory $factoryElement,
+        \Magento\Framework\Data\Form\Element\CollectionFactory $factoryCollection,
+        \Magento\Framework\Escaper $escaper,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        array $data
+    ) {
+    
+        $this->imageModel = $imageModel;
+        parent::__construct($factoryElement, $factoryCollection, $escaper, $urlBuilder, $data);
+    }
+
+    /**
+     * Get image preview url
+     *
+     * @return string
+     */
+    protected function _getUrl()
+    {
+        $url = false;
+        if ($this->getValue()) {
+            $url = $this->imageModel->getBaseUrl().$this->getValue();
+        }
+        return $url;
+    }
+}
---
+++ Model/Author/Image.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Blog
+ * @copyright   Copyright (c) 2016 Mageplaza (http://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+namespace Mageplaza\Blog\Model\Author;
+
+class Image
+{
+	/**
+	 * Media sub folder
+	 *
+	 * @var string
+	 */
+	public $subDir = 'mageplaza/blog/author';
+
+	/**
+	 * URL builder
+	 *
+	 * @var \Magento\Framework\UrlInterface
+	 */
+	public $urlBuilder;
+
+	/**
+	 * File system model
+	 *
+	 * @var \Magento\Framework\Filesystem
+	 */
+	public $fileSystem;
+
+	/**
+	 * constructor
+	 *
+	 * @param \Magento\Framework\UrlInterface $urlBuilder
+	 * @param \Magento\Framework\Filesystem $fileSystem
+	 */
+	public function __construct(
+		\Magento\Framework\UrlInterface $urlBuilder,
+		\Magento\Framework\Filesystem $fileSystem
+	) {
+
+		$this->urlBuilder = $urlBuilder;
+		$this->fileSystem = $fileSystem;
+	}
+
+	/**
+	 * get images base url
+	 *
+	 * @return string
+	 */
+	public function getBaseUrl()
+	{
+		return $this->urlBuilder->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA])
+			.$this->subDir.'/image';
+	}
+	/**
+	 * get base image dir
+	 *
+	 * @return string
+	 */
+	public function getBaseDir()
+	{
+		return $this->fileSystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA)
+			->getAbsolutePath($this->subDir.'/image');
+	}
+}
