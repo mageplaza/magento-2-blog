d06591224d6afbfc034668cda369eefadc96cfa3
haitv282 <haitv282@gmail.com>
2017-11-07 14:40:43 +0700

    related post

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -18,15 +18,10 @@
  * @license     https://www.mageplaza.com/LICENSE.txt
  */
 
-
 var config = {
     paths: {
-        fancybox: 'Mageplaza_Blog/js/lib/jquery.fancybox-1.3.4.pack.min',
         comment: 'Mageplaza_Blog/js/comment',
         autocomplete: 'Mageplaza_Blog/js/lib/jquery.autocomplete.min',
         categoryTree: 'Mageplaza_Blog/js/categorytree'
-    },
-    shim:{
-        fancybox: ['jquery']
     }
 };
