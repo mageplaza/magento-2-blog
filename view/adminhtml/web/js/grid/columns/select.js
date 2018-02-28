63a29a0c9eb94309042016efa9b1ce1b34692013
kenny <hoangnd@mageplaza.com>
2018-02-28 09:54:05 +0700

    - add status color in comment grid admin

---
+++ view/adminhtml/web/js/grid/columns/select.js
@@ -0,0 +1,49 @@
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license sliderConfig is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Blog
+ * @copyright   Copyright (c) 2018 Mageplaza (http://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+define([
+    'Magento_Ui/js/grid/columns/select'
+], function (Column) {
+    'use strict';
+
+    return Column.extend({
+        defaults: {
+            bodyTmpl: 'ui/grid/cells/html'
+        },
+        getLabel: function (record) {
+            var label = this._super(record);
+            console.log(record.status);
+            if (label !== '') {
+                switch (record.status) {
+                    case '1':
+                        label = '<span style="color:green;"><span>' + label + '</span></span>';
+                        break;
+                    case '2':
+                        label = '<span style="color:red;"><span>' + label + '</span></span>';
+                        break;
+                    case '3':
+                        label = '<span style="color:#f08c0a;"><span>' + label + '</span></span>';
+                        break;
+                }
+            }
+            return label;
+        }
+    });
+});
