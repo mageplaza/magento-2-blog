20be5633bd3accfabfcc7b1fd2378a98bd987fdf
eric <thainc@mageplaza.com>
2019-10-24 11:48:25 +0700

    - Post Form frontend  + signup form

---
+++ Controller/Author/Signup.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Blog
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Blog\Controller\Author;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\ForwardFactory;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+use Mageplaza\Blog\Helper\Data;
+use Mageplaza\Blog\Model\Config\Source\SideBarLR;
+
+/**
+ * Class View
+ * @package Mageplaza\Blog\Controller\Author
+ */
+class Signup extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    public $resultPageFactory;
+
+    /**
+     * @type ForwardFactory
+     */
+    protected $resultForwardFactory;
+
+    /**
+     * @var Data
+     */
+    protected $_helperBlog;
+
+    /**
+     * View constructor.
+     *
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     * @param ForwardFactory $resultForwardFactory
+     * @param Data $helperData
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        ForwardFactory $resultForwardFactory,
+        Data $helperData
+    ) {
+        $this->_helperBlog = $helperData;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->resultForwardFactory = $resultForwardFactory;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return $this|Page
+     */
+    public function execute()
+    {
+//        $id = $this->getRequest()->getParam('id');
+        $page = $this->resultPageFactory->create();
+        $page->getConfig()->setPageLayout(SideBarLR::LEFT);
+        $page->getConfig()->getTitle()->set('Signup Author');
+
+        return $page;
+    }
+}
