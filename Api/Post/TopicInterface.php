854ca63ab6bf93b30c836db6be2b93dc6495ba5b
Eric <thainc@mageplaza>
2019-11-05 00:48:48 +0700

    Api and Data

---
+++ Api/Post/TopicInterface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Blog
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Blog\Api\Post;
+
+/**
+ * Class TopicInterface
+ * @package Mageplaza\Blog\Api\Post
+ */
+interface TopicInterface
+{
+    /**
+     * Get post list
+     *
+     * @return \Mageplaza\Blog\Api\Post\PostInterface[]
+     */
+    public function getPostList();
+}
