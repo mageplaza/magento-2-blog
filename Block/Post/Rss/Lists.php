mageplaza/magento-2-blog

commit 68ba215c9f56026de31a705092abe7c3b039930f

v3.0.6 (#205)
* - Fix bug not save Post Magento 2.3.0
- Fix bug not view author when not save Post, Tag, Toppic
- Fix bug 404 when connect to post from store view has disable in config

* - fix bug Multiple store

* - fix bug in Magento 2.2.8

* fix rss error in Magento 2.3.0

* fix rss error in Magento 2.3.0

* - fix to magento 2.3.1

* - fix default comment

* fix bug

* review - clear code

* review-code

* Fix multi store display

* fix bug compile

* Clean code

Block/Post/Rss/Lists.php
@@ -23,9 +23,13 @@ namespace Mageplaza\Blog\Block\Post\Rss;
 
 use Magento\Framework\App\Rss\DataProviderInterface;
 use Magento\Framework\App\Rss\UrlBuilderInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Store\Model\StoreManagerInterface;
 use Mageplaza\Blog\Helper\Data;
+use Mageplaza\Blog\Model\Post;
 
 /**
  * Class NewProducts
@@ -34,26 +38,26 @@ use Mageplaza\Blog\Helper\Data;
 class Lists extends AbstractBlock implements DataProviderInterface
 {
     /**
-     * @var \Magento\Framework\App\Rss\UrlBuilderInterface
+     * @var UrlBuilderInterface
      */
     public $rssUrlBuilder;
 
     /**
-     * @var \Magento\Store\Model\StoreManagerInterface
+     * @var StoreManagerInterface
      */
     public $storeManager;
 
     /**
-     * @var \Mageplaza\Blog\Helper\Data
+     * @var Data
      */
     public $helper;
 
     /**
      * Lists constructor.
      *
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Mageplaza\Blog\Helper\Data $helper
-     * @param \Magento\Framework\App\Rss\UrlBuilderInterface $rssUrlBuilder
+     * @param Context $context
+     * @param Data $helper
+     * @param UrlBuilderInterface $rssUrlBuilder
      * @param array $data
      */
     public function __construct(
@@ -70,7 +74,7 @@ class Lists extends AbstractBlock implements DataProviderInterface
     }
 
     /**
-     * @return void
+     * @throws NoSuchEntityException
      */
     protected function _construct()
     {
@@ -93,29 +97,31 @@ class Lists extends AbstractBlock implements DataProviderInterface
     public function getRssData()
     {
         $storeModel = $this->storeManager->getStore($this->getStoreId());
-        $title = __('List Posts from %1', $storeModel->getFrontendName());
+        $title = __('List Posts from %1', $storeModel->getFrontendName())->render();
+        $storeUrl = $this->storeManager->getStore($this->getStoreId())->getBaseUrl(UrlInterface::URL_TYPE_WEB);
         $data = [
             'title'       => $title,
             'description' => $title,
-            'link'        => $this->rssUrlBuilder->getUrl(['store_id' => $this->getStoreId(), 'type' => 'blog_posts']),
+            'link'        => $storeUrl . 'blog/post/rss.xml',
             'charset'     => 'UTF-8',
             'language'    => $this->helper->getConfigValue('general/locale/code', $storeModel),
         ];
 
         $posts = $this->helper->getPostList($this->getStoreId())
             ->addFieldToFilter('in_rss', 1)
-            ->setOrder('post_id', 'DESC');
-        $posts->getSelect()
+            ->setOrder('post_id', 'DESC')
+            ->getSelect()
             ->limit(10);
+        /** @var Post $item */
         foreach ($posts as $item) {
             $item->setAllowedInRss(true);
             $item->setAllowedPriceInRss(true);
 
-            $description = '<table><tr><td style="text-decoration:none;"> ' . $item->getShortDescription() . '</td></tr></table>';
+            $description = $item->getShortDescription();
             $data['entries'][] = [
                 'title'       => $item->getName(),
                 'link'        => $item->getUrl(),
-                'description' => $description,
+                'description' => $description ?: 'no content',
                 'lastUpdate'  => strtotime($item->getPublishDate())
             ];
         }
@@ -125,15 +131,16 @@ class Lists extends AbstractBlock implements DataProviderInterface
 
     /**
      * @return int
+     * @throws NoSuchEntityException
      */
     public function getStoreId()
     {
-        $storeId = (int)$this->getRequest()->getParam('store_id');
-        if ($storeId == null) {
+        $storeId = $this->getRequest()->getParam('store_id');
+        if ($storeId === null) {
             $storeId = $this->storeManager->getStore()->getId();
         }
 
-        return $storeId;
+        return (int) $storeId;
     }
 
     /**

commit 6f17835e4c90b7733989653674641403251e2758

Refactored loading recent posts from getRssData into separate method.
Refactored the logic for loading recent posts from the \Mageplaza\Blog\Block\Post\Rss\Lists::getRssData
method into separate one, with type-hinted return value.

This should make bugs, like the one introduced in 68ba215c9f56026de31a705092abe7c3b039930f,
easier to detect during the development phase. The aforementioned bug was caused due to
wrong object being assigned to the $posts variable - instead of holding the instance of
Collection, it held an instance of Select. As such, the items of Collection (here Posts)
were never really loaded and the RSS feed was always empty.

Having a type-hinted return value should make it easier to spot the issue during development
in the IDE, or even just by having PHP throw an error when the method returns wrong type.

Block/Post/Rss/Lists.php
@@ -30,6 +30,7 @@ use Magento\Framework\View\Element\Template\Context;
 use Magento\Store\Model\StoreManagerInterface;
 use Mageplaza\Blog\Helper\Data;
 use Mageplaza\Blog\Model\Post;
+use Mageplaza\Blog\Model\ResourceModel\Post\Collection as PostCollection;
 
 /**
  * Class NewProducts
@@ -91,12 +92,26 @@ class Lists extends AbstractBlock implements DataProviderInterface
         return $this->helper->isEnabled();
     }
 
+    private function loadRecentPosts(int $storeId, int $numberOfPosts): PostCollection
+    {
+        /** @var PostCollection $posts */
+        $posts = $this->helper->getPostList($storeId)
+            ->addFieldToFilter('in_rss', 1)
+            ->setOrder('post_id', 'DESC');
+
+        $posts->getSelect()
+            ->limit($numberOfPosts);
+
+        return $posts;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getRssData()
     {
-        $storeModel = $this->storeManager->getStore($this->getStoreId());
+        $storeId = $this->getStoreId();
+        $storeModel = $this->storeManager->getStore($storeId);
         $title = __('List Posts from %1', $storeModel->getFrontendName())->render();
         $storeUrl = $this->storeManager->getStore($this->getStoreId())->getBaseUrl(UrlInterface::URL_TYPE_WEB);
         $data = [
@@ -107,10 +122,9 @@ class Lists extends AbstractBlock implements DataProviderInterface
             'language'    => $this->helper->getConfigValue('general/locale/code', $storeModel),
         ];
 
-        $posts = $this->helper->getPostList($this->getStoreId())
-            ->addFieldToFilter('in_rss', 1)
-            ->setOrder('post_id', 'DESC');
-        $posts->getSelect()->limit(10);
+        /** @var PostCollection $posts */
+        $posts = $this->loadRecentPosts($storeId, 10);
+
         /** @var Post $item */
         foreach ($posts->getItems() as $item) {
             $item->setAllowedInRss(true);