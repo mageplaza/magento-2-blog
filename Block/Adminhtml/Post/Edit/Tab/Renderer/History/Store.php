2501659e05d9b0f576cb8fde791c67d8adda1c18
chuccv <chuccv@mageplaza.com>
2023-05-17 15:09:50 +0700

    [fix] - multiple Store Post/History

--- Block/Adminhtml/Post/Edit/Tab/Renderer/History/Store.php
+++ Block/Adminhtml/Post/Edit/Tab/Renderer/History/Store.php
@@ -14,78 +14,98 @@
  * version in the future.
  *
  * @category    Mageplaza
- * @package     Mageplaza_Blog
+ * @package     Mageplaza_LayeredNavigationUltimate
  * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
  * @license     https://www.mageplaza.com/LICENSE.txt
  */
 
 namespace Mageplaza\Blog\Block\Adminhtml\Post\Edit\Tab\Renderer\History;
 
-use Magento\Backend\Block\Widget\Grid\Column\Renderer\Store as CoreStore;
+use Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer;
 use Magento\Framework\DataObject;
-use Magento\Framework\Phrase;
+use Magento\Framework\Escaper;
+use Magento\Store\Model\System\Store as SystemStore;
 
 /**
- * Class Store
- * @package Mageplaza\Blog\Block\Adminhtml\Post\Edit\Tab\Renderer\History
+ * Class Name
+ * @package Mageplaza\LayeredNavigationUltimate\Block\Adminhtml\Grid\Renderer
  */
-class Store extends CoreStore
+class Store extends AbstractRenderer
 {
     /**
-     * Render row store views
+     * @var Escaper
+     */
+    protected $escaper;
+
+    /**
+     * System store
+     *
+     * @var SystemStore
+     */
+    protected $systemStore;
+
+    /**
+     * Store constructor.
+     *
+     * @param SystemStore $systemStore
+     * @param Escaper $escaper
+     */
+    public function __construct(
+        SystemStore $systemStore,
+        Escaper $escaper
+    ) {
+        $this->systemStore = $systemStore;
+        $this->escaper     = $escaper;
+    }
+
+    /**
+     * Renders grid column
      *
      * @param DataObject $row
      *
-     * @return Phrase|string
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
+     * @return  string
      */
     public function render(DataObject $row)
     {
-        $out = '';
-        $skipAllStoresLabel = $this->_getShowAllStoresLabelFlag();
-        $skipEmptyStoresLabel = $this->_getShowEmptyStoresLabelFlag();
-        $origStores = $row->getData($this->getColumn()->getIndex());
+        if ($row) {
+            $store  = '';
+            $store .= $this->prepareItem($row->getData());
 
-        if ($origStores === '0') {
-            $origStores = [0 => '0'];
+            return $store;
         }
 
-        if ($origStores === null && $row->getStoreName()) {
-            $scopes = [];
-            foreach (explode("\n", $row->getStoreName()) as $k => $label) {
-                $scopes[] = str_repeat('&nbsp;', $k * 3) . $label;
-            }
-            $out .= implode('<br/>', $scopes) . __(' [deleted]');
+        return '';
+    }
 
-            return $out;
+    /**
+     * @param array $item
+     * @return \Magento\Framework\Phrase|string
+     */
+    protected function prepareItem(array $item)
+    {
+        $content    = '';
+        $origStores = [];
+        $items      = explode(",", $item['store_ids']);
+
+        foreach ($items as $storeId) {
+            $origStores[] = $storeId;
         }
 
-        if (empty($origStores) && !$skipEmptyStoresLabel) {
-            return '';
-        }
-        if (!is_array($origStores)) {
-            $origStores = [$origStores];
-        }
-
-        if (empty($origStores)) {
-            return '';
-        } elseif (!$skipAllStoresLabel && in_array('0', $origStores, true) && count($origStores) === 1) {
+        if (in_array(0, $origStores)) {
             return __('All Store Views');
         }
 
-        $data = $this->_getStoreModel()->getStoresStructure(false, $origStores);
-
+        $data = $this->systemStore->getStoresStructure(false, $origStores);
         foreach ($data as $website) {
-            $out .= $website['label'] . '<br/>';
+            $content .= "<b>" . $website['label'] . "</b><br/>";
             foreach ($website['children'] as $group) {
-                $out .= str_repeat('&nbsp;', 3) . $group['label'] . '<br/>';
+                $content .= str_repeat('&nbsp;', 3) . "<b>" . $this->escaper->escapeHtml($group['label']) . "</b><br/>";
                 foreach ($group['children'] as $store) {
-                    $out .= str_repeat('&nbsp;', 6) . $store['label'] . '<br/>';
+                    $content .= str_repeat('&nbsp;', 6) . $this->escaper->escapeHtml($store['label']) . "<br/>";
                 }
             }
         }
 
-        return $out;
+        return $content;
     }
 }
